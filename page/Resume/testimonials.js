07ed15a3dd825e3fa20c8fd2e929214e4b307d86
Trần Trung Kiên <trungkien2981412@gmail.com>
2025-05-21 09:56:55 +0700

    Xây dựng tab RESUME

---
+++ page/Resume/testimonials.js
@@ -0,0 +1,20 @@
+const testimonials = [
+  {
+    name: "Jack Garratt",
+    role: "Freelancer",
+    avatar: "img/user1.jpg",
+    text: "Great designer! He finished our work wonderfully and just in time. Thanks for everything."
+  },
+  {
+    name: "April M. Griffin",
+    role: "Founder",
+    avatar: "img/user2.jpg",
+    text: "Our logo and business card design look great. Thanks Henry."
+  },
+  {
+    name: "Larry M. Johnson",
+    role: "Freelancer",
+    avatar: "img/user3.jpg",
+    text: "Henry is a very creative and talented designer. I do not hesitate to work again."
+  }
+];
